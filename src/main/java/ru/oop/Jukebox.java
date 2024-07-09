73503c65af097bc4804e623503f51ee0b3075230
TrFilipp <trishin989898@gmail.com>
2024-07-09 20:07:03 +0300

    Задание 7.4 Вызов метода с аргументами

---
+++ src/main/java/ru/oop/Jukebox.java
@@ -0,0 +1,27 @@
+package ru.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        switch (position) {
+            case 1: {
+                System.out.println("Пусть бегут неуклюже");
+                break;
+            }
+            case 2: {
+                System.out.println("Спокойной ночи");
+                break;
+            }
+            default: {
+                System.out.println("Песня не найдена");
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox song = new Jukebox();
+        song.music(2);
+        song.music(3);
+        song.music(1);
+    }
+}
