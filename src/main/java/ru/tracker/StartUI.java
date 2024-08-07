82e2297c8f71196ee91ee55f1c0694ca68dcfebf
TrFilipp <trishin989898@gmail.com>
2024-08-08 00:04:52 +0300

    Задание 8.7 Date. Отображение даты

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -4,9 +4,9 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item dateTimeNow = new Item();
+        Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeNow = dateTimeNow.getCreated().format(formatter);
-        System.out.println("Текущее дата и время: " + currentDateTimeNow);
+        String formatItem = item.getCreated().format(formatter);
+        System.out.println("Текущее дата и время: " + formatItem);
     }
 }
