081557fe4f2919ee57c4b324789b00268cfb9ec1
TrFilipp <trishin989898@gmail.com>
2024-08-11 09:35:52 +0300

    Задание 9.3 Массивы и модели

---
+++ src/main/java/ru/pojo/Library.java
@@ -0,0 +1,27 @@
+package ru.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book romane = new Book("Ромео и Джульета", 400);
+        Book horror = new Book("Оно", 350);
+        Book comedy = new Book("Недоросль", 231);
+        Book cleanCode = new Book("Clean code", 111);
+        Book[] library = new Book[4];
+        library[0] = romane;
+        library[1] = horror;
+        library[2] = comedy;
+        library[3] = cleanCode;
+        for (int i = 0; i < library.length; i++) {
+            Book book = library[i];
+            System.out.println(book.getName() + " - " + book.getPages());
+        }
+        System.out.println("Replace book romance and Clean code");
+        Book temp = library[0];
+        library[0] = library[3];
+        library[3] = temp;
+        for (int i = 0; i < library.length; i++) {
+            Book book = library[i];
+            System.out.println(book.getName() + " - " + book.getPages());
+        }
+    }
+}
