386a09196b76370d116b84bd89f936751a4a31b4
TrFilipp <trishin989898@gmail.com>
2024-08-20 19:57:02 +0300

    Задание 9.5 Удаление моделей из массива

---
+++ src/main/java/ru/pojo/ShopDrop.java
@@ -0,0 +1,15 @@
+package ru.pojo;
+
+public class ShopDrop {
+    public static Product[] delete(Product[] products, int index) {
+        for (int i = index; i < products.length; i++) {
+            if (i == products.length - 1) {
+                products[i] = null;
+                break;
+            } else {
+                products[i] = products[i + 1];
+            }
+        }
+        return products;
+    }
+}
