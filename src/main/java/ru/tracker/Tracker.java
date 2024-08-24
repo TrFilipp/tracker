dd1c2238a6e15e042c4af6691857f63d67a5de3e
TrFilipp <trishin989898@gmail.com>
2024-08-24 14:45:51 +0300

    Задание 9.7 Tracker - хранилище

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,34 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        int sizeOfRsl = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item name = items[i];
+            if (name != null) {
+                rsl[sizeOfRsl] = name;
+                sizeOfRsl++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, sizeOfRsl);
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int sizeOfRsl = 0;
+        for (int i = 0; i < size; i++) {
+            Item name = items[i];
+            if (key.equals(name.getName())) {
+                rsl[sizeOfRsl] = name;
+                sizeOfRsl++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, sizeOfRsl);
+        return rsl;
     }
 
     public Item findById(int id) {
