9834c88ce51116694972c6140d50b9f40b118e4c
TrFilipp <trishin989898@gmail.com>
2024-09-06 19:07:30 +0300

    Задание 9.9 Метод удаления заявки. Tracker.delete

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -55,4 +55,13 @@ public class Tracker {
         int index = indexOf(id);
         return index != -1 ? items[index] : null;
     }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+    }
 }
