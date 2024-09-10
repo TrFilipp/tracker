34843c36941988e14ff3063e4b4d774b85914d71
TrFilipp <trishin989898@gmail.com>
2024-09-10 18:44:03 +0300

    Задание 9.12 toString()

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -4,7 +4,7 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item = new Item(10, "test");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String formatDateItem = item.getCreated().format(formatter);
         System.out.println("Дата и время создания заявки: " + formatDateItem);
