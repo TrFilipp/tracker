6b489abcf17ae98463a9b5ea16418bd1594e504d
TrFilipp <trishin989898@gmail.com>
2024-10-15 17:31:08 +0300

    Задание 10.13 Input

--- src/main/java/ru/poly/Bus.java
+++ src/main/java/ru/poly/Bus.java
@@ -4,12 +4,12 @@ public class Bus implements Transport {
 
     @Override
     public void move() {
-
+        System.out.println("Машина двигается");
     }
 
     @Override
     public void passengers(int passenger) {
-
+        System.out.println("Количество пассажиров в транспорте: " + passenger);
     }
 
     @Override
