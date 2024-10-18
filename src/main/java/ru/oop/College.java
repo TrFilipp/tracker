81b444cf01595f1b9009173a4f92e36f8102a575
TrFilipp <trishin989898@gmail.com>
2024-10-18 14:41:54 +0300

    Задание 10.14 Приведение типов

---
+++ src/main/java/ru/oop/College.java
@@ -0,0 +1,9 @@
+package ru.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman frsh = new Freshman();
+        Student std = frsh;
+        Object obj = std;
+    }
+}
