08cbb177fd82c7df29aa417604e994b4ce043542
TrFilipp <trishin989898@gmail.com>
2024-11-25 22:00:38 +0300

    Задание 11.6 Иерархия исключений

---
+++ src/main/java/ru/ex/UserStore.java
@@ -0,0 +1,44 @@
+package ru.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User rsl = new User("null", false);
+        for (int i = 0; i < users.length; i++) {
+            User user = users[i];
+            if (login.equals(user.getUsername())) {
+                rsl = users[i];
+                break;
+            }
+        }
+        if (!rsl.isValid()) {
+            throw new UserNotFoundException("User not found");
+        }
+        return rsl;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        boolean rsl = true;
+        String username = user.getUsername();
+        if (user.isValid() && username.length() < 3) {
+            throw new UserInvalidException("User not validate");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Ivan Ivanov", true)
+        };
+        try {
+            User user = findUser(users, "Ivan Ivanov");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException i) {
+            i.printStackTrace();
+        } catch (UserNotFoundException nf) {
+            nf.printStackTrace();
+        }
+    }
+}
