452bfdb7c7bc4dc16eeaa19fd9bfdc32a4a8b574
TrFilipp <trishin989898@gmail.com>
2025-03-14 15:15:41 +0300

    Задание 16.5 Аттестация

--- src/main/java/ru/hashmap/AnalyzeByMap.java
+++ src/main/java/ru/hashmap/AnalyzeByMap.java
@@ -1,13 +1,29 @@
 package ru.hashmap;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AnalyzeByMap {
     public static double averageScore(List<Pupil> pupils) {
-        for (Pupil pupil: pupils) {
-            pupil.subjects();
+        List<Integer> scores = new ArrayList<>();
+        double sum = 0;
+        double sumAll = 0;
+        int subjectAll = 0;
+
+        for (Pupil pupil : pupils) {
+            pupil.subjects().size();
+            for (Subject subject : pupil.subjects()) {
+                scores.add(subject.score());
+                subjectAll++;
+            }
+            for (int i = 0; i < scores.size(); i++) {
+                sum += scores.get(i);
+            }
+            sum /= subjectAll;
+            sumAll += sum;
+            sum = 0;
         }
-        return 0D;
+        return sumAll;
     }
 
     public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
