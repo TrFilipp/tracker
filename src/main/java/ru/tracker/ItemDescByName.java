bbd3ad3a84be314c05ef9be1c184d63189ef0a18
TrFilipp <trishin989898@gmail.com>
2025-03-26 12:50:07 +0300

    Задание 17.1 Сортировка

--- src/main/java/ru/tracker/ItemDescByName.java
+++ src/main/java/ru/tracker/ItemDescByName.java
@@ -2,7 +2,7 @@ package ru.tracker;
 
 import java.util.Comparator;
 
-public class ItemDescByName implements Comparator<Item>{
+public class ItemDescByName implements Comparator<Item> {
     @Override
     public int compare(Item o1, Item o2) {
         return o2.getName().compareTo(o1.getName());
