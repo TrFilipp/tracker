04bba9e0a019a83c1d68cab535ff3366cc8b7072
TrFilipp <trishin989898@gmail.com>
2025-03-27 11:20:48 +0300

    Задание 17.2 Организовать сортировку User

---
+++ src/main/java/ru/collection/User.java
@@ -0,0 +1,39 @@
+package ru.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        return o.getName().compareTo(name) == 0 ? Integer.compare(age, o.getAge()) : name.compareTo(o.getName());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
