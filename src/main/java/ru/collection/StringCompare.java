09b023c78b531932e9ff73828d2dea1cebec8440
TrFilipp <trishin989898@gmail.com>
2025-04-15 09:09:55 +0300

    Задание 17.4 Компаратор для строк

--- src/main/java/ru/collection/StringCompare.java
+++ src/main/java/ru/collection/StringCompare.java
@@ -6,13 +6,14 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int rsl = 0;
+        if (left.length() != right.length()) {
+            rsl = Integer.compare(left.length(), right.length());
+        }
         for (int i = 0; i < Math.min(left.length(), right.length()) - 1; i++) {
+
             if (left.charAt(i) != right.charAt(i)) {
                 rsl = Character.compare(left.charAt(i), right.charAt(i));
                 break;
-            }
-            if (left.length() != right.length()) {
-                rsl = Integer.compare(left.length(), right.length());
             }
         }
         return rsl;
