1766ac18b43748897f0ee841034646895192b9cb
TrFilipp <trishin989898@gmail.com>
2025-04-30 10:58:14 +0300

    Задание 19.1 Отсортировать департаменты

---
+++ src/main/java/ru/collection/Departments.java
@@ -0,0 +1,22 @@
+package ru.collection;
+
+import java.util.LinkedHashSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> departments) {
+        Set<String> temp = new LinkedHashSet<>();
+        return null;
+    }
+
+    public static void sortAsc(List<String> departments) {
+
+    }
+
+    public static void sortDesc(List<String> departments) {
+
+    }
+}
