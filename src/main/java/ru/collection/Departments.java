b3ba9ed5bfd0e3312ebc6b307ccafcaef17e22ba
TrFilipp <trishin989898@gmail.com>
2025-05-03 23:06:04 +0300

    Задание 19.1 Отсортировать департамент

--- src/main/java/ru/collection/Departments.java
+++ src/main/java/ru/collection/Departments.java
@@ -1,22 +1,41 @@
 package ru.collection;
 
-import java.util.LinkedHashSet;
+import java.util.*;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 public class Departments {
 
+    /**
+     * Метод заполняет коллекцию недостающими элементами исходя из полученных данных
+     * @param departments список департаментов
+     * @return возвращает список всех подразделений
+     */
     public static List<String> fillGaps(List<String> departments) {
         Set<String> temp = new LinkedHashSet<>();
-        return null;
+        for (String department : departments) {
+            ArrayList<String> sections = new ArrayList<>(List.of(department.split("/")));
+            for (int i = 1; i <= sections.size(); i++) {
+                temp.add(String.join("/", sections.subList(0, i)));
+            }
+            temp.add(department);
+        }
+        return new ArrayList<>(temp);
     }
 
+    /**
+     * Метод сортирует список по возрастанию
+     * @param departments список департаментов
+     */
     public static void sortAsc(List<String> departments) {
-
+        departments.sort(Comparator.naturalOrder());
     }
 
+    /**
+     * Метод сортирует список по убыванию, используя компаратор DepartmentsDescComparator
+     * @param departments список департаментов
+     */
     public static void sortDesc(List<String> departments) {
-
+        DepartmentsDescComparator descComparator = new DepartmentsDescComparator();
+        departments.sort(descComparator);
     }
 }
--- src/main/java/ru/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/collection/DepartmentsDescComparator.java
@@ -1,10 +1,29 @@
 package ru.collection;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
+/**
+ * Класс реализует переопределённый метод для сравнения строк, благодаря которому будет произведена сортировка по убывающему
+ * @author trishin_f
+ * @version 1.0
+ */
 public class DepartmentsDescComparator implements Comparator<String> {
+    /**
+     * Переопределённый метод для сравнения строк
+     * @param left the first object to be compared.
+     * @param right the second object to be compared.
+     * @return Возвращает числовое значение в виде разницы двух строк
+     */
     @Override
     public int compare(String left, String right) {
-        return right.compareTo(left);
+        ArrayList<String> tempLeft = new ArrayList<>(List.of(left.split("/")));
+        ArrayList<String> tempRight = new ArrayList<>(List.of(right.split("/")));
+        int rsl = tempRight.get(0).compareTo(tempLeft.get(0));
+        if (tempRight.get(0).compareTo(tempLeft.get(0)) == 0)  {
+            rsl = left.compareTo(right);
+        }
+        return rsl;
     }
 }
